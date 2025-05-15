3c7fa8ccaef25c6bb57a2d601ae05f8c7cccd8a6
Luis Daniel Castañeda Abanto <131382266+castanedad121@users.noreply.github.com>
2025-05-15 15:22:07 -0500

    version estable

--- src/hooks/getParamsEnv.js
+++ src/hooks/getParamsEnv.js
@@ -1,9 +1,13 @@
 //! Unico lugar donde obtengo las variables de entorno.
 export default function getParamsEnv() {
-  const SECURE = import.meta.env.SECURE === "true";
+  console.log(import.meta.env.SECURE);
+  console.log(import.meta.env.API_URL_BASE);
+  const SECURE = import.meta.env.SECURE === "TRUE";
   const API_URL_BASE = SECURE
     ? import.meta.env.API_URL_BASE
     : "http://localhost:4242/";
+
+  console.log(API_URL_BASE);
 
   return {
     SECURE,
