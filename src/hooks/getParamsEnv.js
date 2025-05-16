834b1d4cd16e47b1ec31eb6c0e9f56891a597dc3
Luis Daniel Castañeda Abanto <131382266+castanedad121@users.noreply.github.com>
2025-05-16 11:44:52 -0500

    Rename Environment Variables

--- src/hooks/getParamsEnv.js
+++ src/hooks/getParamsEnv.js
@@ -1,10 +1,10 @@
 //! Unico lugar donde obtengo las variables de entorno.
 export default function getParamsEnv() {
-  console.log(import.meta.env.SECURE);
-  console.log(import.meta.env.API_URL_BASE);
-  const SECURE = import.meta.env.SECURE === "TRUE";
+  console.log(import.meta.env.VITE_SECURE);
+  console.log(import.meta.env.VITE_API_URL_BASE);
+  const SECURE = import.meta.env.VITE_SECURE === "TRUE";
   const API_URL_BASE = SECURE
-    ? import.meta.env.API_URL_BASE
+    ? import.meta.env.VITE_API_URL_BASE
     : "http://localhost:4242/";
 
   console.log(API_URL_BASE);
